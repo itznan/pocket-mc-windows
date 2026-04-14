576a03d8896a35d335a14e29e42c929845ac4bad
Sahaj33-op <sahajitaliya07@gmail.com>
2026-04-14 17:57:29 +0530

    feat: add global usings to prevent missing-using build errors

---
+++ PocketMC.Desktop/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global usings to reduce missing-using build errors across the project
+global using PocketMC.Desktop.Features.Instances.Models;
+global using PocketMC.Desktop.Features.Instances.Services;
+global using PocketMC.Desktop.Features.Instances.Providers;
+global using PocketMC.Desktop.Features.Mods;
